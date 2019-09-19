ce23dc50d897d18fba3e7cf09f5dc633c3019191
Mike Chaberski <mchaberski@gmail.com>
2019-09-19 15:48:27 -0400

    add test for string directcontent

--- src/test/java/io/github/mike10004/subprocess/StreamContentsTest.java
+++ src/test/java/io/github/mike10004/subprocess/StreamContentsTest.java
@@ -7,8 +7,14 @@ import static org.junit.Assert.*;
 public class StreamContentsTest {
 
     @Test
-    public void testToString() {
+    public void testToString_byteArrays() {
         StreamContent<?, ?> p = StreamContent.direct(new byte[3], new byte[7]);
         assertEquals("toString()", "DirectOutput{stdout=byte[3], stderr=byte[7]}", p.toString());
+    }
+
+    @Test
+    public void testToString_strings() {
+        StreamContent<?, ?> p = StreamContent.direct("blah", null);
+        assertEquals("toString()", "DirectOutput{stdout=\"blah\", stderr=}", p.toString());
     }
 }
